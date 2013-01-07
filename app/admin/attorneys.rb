e6b4b5c8584be84dc22e10ae9954c9e01646656e
Taylor Kearns <tkearns@gmail.com>
2013-01-06 21:44:56 -0500

    Updates to Attorney model and admin

--- app/admin/attorneys.rb
+++ app/admin/attorneys.rb
@@ -1,3 +1,10 @@
 ActiveAdmin.register Attorney do
-  
+  form do |f|
+    f.inputs do
+      f.input :first_name
+      f.input :last_name
+      f.input :practice_areas, as: :check_boxes, collection: PracticeArea.order('name')
+    end
+    f.buttons
+  end
 end
--- app/models/attorney.rb
+++ app/models/attorney.rb
@@ -1,7 +1,13 @@
 class Attorney < ActiveRecord::Base
-  attr_accessible :first_name, :last_name, :email, :list_order
+  attr_accessible :first_name, :last_name, :email, :list_order,
+    :practice_area_ids
 
+  has_many :attorneys_practice_areas
   has_many :practice_areas, through: :attorneys_practice_areas
 
   scope :ordered, order('list_order')
+
+  def to_s
+    "#{first_name} #{last_name}"
+  end
 end
