b403be42fc4d2112030afaf9660e57c9d58563a5
Taylor Kearns <tkearns@gmail.com>
2013-01-06 21:45:21 -0500

    Updates to PracticeArea model, admin, and view

--- app/admin/practice_areas.rb
+++ app/admin/practice_areas.rb
@@ -2,8 +2,9 @@ ActiveAdmin.register PracticeArea do
   form do |f|
     f.inputs do
       f.input :name
-      f.input :overview
-      f.input :attorneys, as: :select
+      f.input :overview, input_html: { class: 'tinymce' }
+      f.input :attorneys, as: :check_boxes, collection: Attorney.order('last_name')
     end
+    f.buttons
   end
 end
--- app/models/practice_area.rb
+++ app/models/practice_area.rb
@@ -1,10 +1,15 @@
 class PracticeArea < ActiveRecord::Base
-  attr_accessible :name, :overview
+  attr_accessible :name, :overview, :attorney_ids
 
   has_many :articles
   has_many :links
 
+  has_many :attorneys_practice_areas
   has_many :attorneys, through: :attorneys_practice_areas
 
   scope :ordered, order('name')
+
+  def practicing_attorneys
+    attorneys.ordered
+  end
 end
