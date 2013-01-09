2f4567c4c1e4c715365232fb80f17bc98f35ea9a
Taylor Kearns <tkearns@gmail.com>
2013-01-09 17:12:49 -0500

    PracticeArea spec and factory

---
+++ spec/factories/practice_area.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :practice_area do
+    name 'Sometypeof Law'
+    overview 'Lawyer ipsum dolor sue me amet.'
+  end
+end
--- spec/models/practice_area_spec.rb
+++ spec/models/practice_area_spec.rb
@@ -1,5 +1,46 @@
 require 'spec_helper'
 
 describe PracticeArea do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'with no practicing attorneys' do
+    subject(:practice_area) { FactoryGirl.build :practice_area }
+
+    it 'should have an attorney practices count of 0' do
+      practice_area.attorney_practices.count.should == 0
+    end
+  end
+
+  context 'with one practicing attorney' do
+    let(:practice_area) { FactoryGirl.build :practice_area }
+    let(:attorney) { FactoryGirl.build :attorney }
+    let(:attorney_practice) do
+      FactoryGirl.build :attorney_practice, attorney: attorney, practice_area: practice_area
+    end
+
+    it 'should have an attorney practices count of 1' do
+      practice_area.attorney_practices.count.should == 1
+    end
+  end
+
+  context 'with some practicing attorneys' do
+    #let!(:practicing_attorney_1) { FactoryGirl.build :attorney }
+    #let!(:practicing_attorney_2) { FactoryGirl.build :attorney }
+    #let!(:non_practicing_attorney) { FactoryGirl.build :attorney }
+    #let!(:practice_area) { FactoryGirl.build :practice_area }
+
+    #let(:attorney_practice) do
+      #FactoryGirl.build :attorney_practice,
+        #attorney: practicing_attorney_1,
+        #practice_area: practice_area
+    #end
+
+    #let(:attorney_practice) do
+      #FactoryGirl.build :attorney_practice,
+        #attorney: practicing_attorney_2,
+        #practice_area: practice_area
+    #end
+
+    #it 'should have some but not all attorneys practicing it' do
+      #practice_area.attorney_practices.count.should == 2
+    #end
+  end
 end
