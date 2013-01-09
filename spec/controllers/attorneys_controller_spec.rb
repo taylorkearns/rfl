e2852ba5e017356fca1ea38434e06b1826e63fca
Taylor Kearns <tkearns@gmail.com>
2013-01-09 17:11:26 -0500

    Re-implemented RSpec

--- spec/controllers/attorneys_controller_spec.rb
+++ spec/controllers/attorneys_controller_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
 describe AttorneysController do
-
+  pending
 end
--- spec/models/attorney_practice_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe AttorneyPractice do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
